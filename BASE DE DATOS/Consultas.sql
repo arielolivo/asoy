b7e67c3bd389b275ee6fa2ab71befa2f55d47152
Alumno <arielolivoy@gmail.com>
2019-02-07 08:27:30 +0100

    Consultas

---
+++ BASE DE DATOS/Consultas.sql
@@ -0,0 +1,50 @@
+--1. Mostrar el apellido, fecha de alta y número de departamento de cada empleado.
+--2. Mostrar el número, nombre y localización de cada departamento.
+--3. Mostrar todos los datos de todos los empleados.
+--4. Datos de los empleados ordenados por apellido.
+--5. Datos de los empleados ordenados descendentemente por número de departamento.
+--6. Datos de los empleados ordenados descendentemente por número de departamento
+--y dentro de cada departamento ordenados ascendentemente por apellido.
+--7. Mostrar los datos de los empleados cuyo salario esté comprendido entre 10000 y 30000,
+--ordenados por salario.
+--8. Mostrar los datos de los empleados cuyo salario sea mayor que 20000.
+--9. Mostrar los datos de los empleados cuyo oficio sea programador.
+--10. Seleccionar el apellido y oficio de los empleados del departamento número 20.
+--11. Mostrar los apellidos de los empleados en orden ascendente.
+--12. Seleccionar los empleados cuyo oficio sea marketing manager. Mostrar los datos
+--ordenados por apellido.
+--13. Mostrar los empleados cuyo departamento sea 10 y cuyo oficio sea accountant.
+--Ordenar el resultado por apellido;
+--14. Mostrar los empleados que tengan un salario mayor que 200000 o que
+--pertenezcan al departamento número 20.
+--15. Ordenar los empleados por oficio, y dentro de oficio por apellido.
+--16. Seleccionar los empleados cuyo apellido empiece por ‘S’.
+--17. Seleccionar los empleados cuyo apellido termine en 'n’.
+--18. Seleccionar de la tabla empleados aquellas filas cuyo apellido empiece por ‘A’ y el
+--email tenga una ‘E’ en cualquier posición.
+--19. Seleccionar los empleados cuyo salario esté entre 10000 y 17000. Utilizar el operador
+--BETWEEN.
+--20. Obtener los empleados que tengan una comisión superior a 600.
+--21. Seleccionar los datos de los empleados ordenados por número de departamento, y
+--dentro de cada departamento ordenados por apellido.
+--22. Número y apellidos de los empleados cuyo email termine en ‘h’ y tengan un salario
+--superior a 2000.
+--23. Datos de los departamentos cuya localización empiece por ‘B’.
+--24. Datos de los empleados que tengan un salario superior a 200 y pertenezcan al
+--departamento número 10.
+--25. Mostrar los apellidos de los empleados que no tengan comisión.
+--26. Mostrar los apellidos de los empleados que no tengan comisión y cuyo apellido
+--empiece por ‘J’.
+--27. Seleccionar el apellido, salario y número de departamento de los empleados cuyo salario
+--sea mayor que 200000.
+--28. Obtener los apellidos de todos los empleados en minúscula.
+--29. En una consulta concatena el apellido de cada empleado con su oficio.
+--30. Mostrar el apellido y la longitud del apellido (función LENGTH) de todos los
+--empleados, ordenados por la longitud de los apellidos de los empleados
+--descendentemente.
+--31. Obtener el año de contratación de todos los empleados (función TO_CHAR).
+--32. Mostrar los datos de los empleados que hayan sido contratados en el año 2004.
+--33. Mostrar los datos de los empleados que hayan sido contratados en el mes de
+--febrero de cualquier año.
+--34. Mostrar los datos de los empleados cuyo apellido empiece por 'S' y hayan sido
+--contratados en el año 2006.
