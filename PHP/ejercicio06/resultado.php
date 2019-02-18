47c4f0c87274eb4e435a10118c038849e2b3391e
Alumno <arielolivoy@gmail.com>
2019-02-18 13:05:56 +0100

    php

--- PHP/ejercicio06/resultado.php
+++ PHP/ejercicio06/resultado.php
@@ -1,3 +1,11 @@
 <?php
+    $nombre= trim(htmlspecialchars( strip_tags($_REQUEST["nombre"]), ENT_QUOTES, "UTF_8"));
+    if ($nombre !=""){
+        print "<p>Su nombre es $nombre</p>";
+    }
+    if (insset($_REQUEST["opcion"])) {
+        print "<p>Opcion marcada</p>"; 
+    }
     print_r($_REQUEST);
+    print_r($_FILES);
 ?>
