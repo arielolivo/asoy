cb007d961166052217771b3675f7c957fe3e9b33
arielolivo <43634584+arielolivo@users.noreply.github.com>
2019-02-20 12:39:03 +0100

    Create consultas_pinacotecas.sql

---
+++ BASE DE DATOS/consultas_pinacotecas.sql
@@ -0,0 +1,21 @@
+--1)Consultar el contenido de todas las tablas.
+
+
+
+--2)Consultar sólo el identificador y el nombre las siguientes tablas:
+--Pinacotecas.
+--Cuadros.
+--Pintores.
+--Mecenas.
+--Escuelas.
+
+--Usando concatenación de textos, se pide listar los datos de la tabla pintores con las siguientes columnas: "IDENTIFICADOR", "NOMBRE", "ID-NOMBRE", donde ID-NOMBRE será el resultado de la concatenación del campo ID_PINTOR y de N_PINTOR.
+--Usando concatenación de textos, listar el contenido de la tabla pinacotecas de la siguiente forma: "Pinacoteca (ID_PINACOTECA): (NOMBRE)".
+--Seleccionar nombre y país de la tabla pinacotecas donde ID_PINACOTECA es "MUBAS".
+--Seleccionar técnica, ancho y alto de los siguientes cuadros: La Gioconda, El nacimiento de Venus y Sagrada Familia.
+--Seleccionar nombre y ciudad de nacimiento de los mecenas italianos.
+--Seleccionar el nombre de los mecenas que nacieron en Madrid o en Valladolid.
+--Seleccionar nombre y fecha de aparición de las escuelas españolas o italianas.
+--Idem anterior pero usando la cláusula IN.
+--Seleccionar nombre y ciudad de nacimiento de los mecenas holandeses y belgas usando la cláusula IN.
+--Seleccionar nombre y ciudad de nacimiento de los mecenas cuyo nombre comienza por F y termina en O.
