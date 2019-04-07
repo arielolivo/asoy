3923d5d9af80f7d5fe2ad2664c8ed78c07f3024b
Ariel_Olivo <43634584+arielolivo@users.noreply.github.com>
2019-04-07 16:04:18 +0200

    Create Programas.sql

---
+++ BASE DE DATOS/Programas.sql
@@ -0,0 +1,13 @@
+--1. Inserta en la tabla "Programas" el sistema operativo Windows 10 Professional.
+--2. Crea una tabla llamada "Microsoft" con la misma estructura que la tabla "Programas".
+--Inserta en ella sólo aquellos programas del fabricante "Microsoft".
+--3. Inserta en la tabla "Registra" las filas correspondientes a los registros que haría el cliente con DNI "5" 
+--para cada uno de los programas que podría comprar en "El Corte Inglés" de Sevilla. Los registros los haría por Internet.
+--4. Añade a la tabla "Programas" una columna llamada "registros" de tipo number.
+--A continuación, para cada programa, escribe en esta columna el número de registros que se han realizado del mismo,
+--independientemente del medio utilizado para ello (Internet, teléfono o Tarjeta postal).
+--5. Añade ahora a la tabla "Programas" tres columnas más: "resgistros_i", "registros_t" y "registros_p". 
+--Actualiza cada una con el número de registros que se han realizado por cada medio (Internet, Teléfono y Tarjeta postal).
+--6. Añade a la tabla "Clientes" una columna llamada "registrados" de tipo number que 
+--deberás actualizar con el número de registros que ha efectuado el cliente,
+--independientemente del medio por el que lo haya hecho.
