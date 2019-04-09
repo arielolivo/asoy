2bfeb23fee789743bc3831d34f388a3e8df8789e
Alumno <arielolivoy@gmail.com>
2019-04-09 13:26:03 +0200

    bs

--- BASE DE DATOS/Olivo_Yepez_Ariel_Practica5_3.sql
+++ BASE DE DATOS/Olivo_Yepez_Ariel_Practica5_3.sql
@@ -11,6 +11,13 @@ create table microsoft(
 	version		varchar2(20)
 	);
 
+INSERT INTO microsoft(codigo,nombre,version)
+    select p.codigo,p.nombre, p.version
+    from programas p 
+        inner join desarrolla d on p.codigo=d.codigo
+        inner join fabricante f on f.id_fab=d.id_fab
+    where f.nombre='Microsoft'
+
 
 --3. Inserta en la tabla "Registra" las filas correspondientes a los registros que haría el cliente con DNI "5" 
 --para cada uno de los programas que podría comprar en "El Corte Inglés" de Sevilla. Los registros los haría por Internet.
@@ -27,17 +34,53 @@ INSERT INTO registra(CIF,DNI,CODIGO,MEDIO)
 --independientemente del medio utilizado para ello (Internet, teléfono o Tarjeta postal).
 
 ALTER TABLE programas ADD registros number(10);
-INSERT INTO programas(registros)
-select count (r.codigo)
-from registra r
-inner join programas  p on p.codigo = r.codigo
-group by r.codigo
+update programas p
+set registros=(
+                select count (r.codigo)
+                from registra r
+                where r.codigo=p.codigo);
 
 --5. Añade ahora a la tabla "Programas" tres columnas más: "resgistros_i", "registros_t" y "registros_p". 
 --Actualiza cada una con el número de registros que se han realizado por cada medio (Internet, Teléfono y Tarjeta postal).
 
+ALTER TABLE programas ADD (registros_i number(10),
+                          registros_t number(10),
+                          registros_p number(10));
 
+--registro i 
+update programas p
+set registros_i=(
+select count(medio)
+from registra r
+where medio='Internet' and r.codigo=p.codigo);
+
+--registros t
+update programas p
+set registros_t=(
+select count(medio)
+from registra r
+where medio='Teléfono' and r.codigo=p.codigo);
+
+--registros p
+update programas p
+set registros_p=(
+select count(medio)
+from registra r
+where medio='Tarjeta postal' and r.codigo=p.codigo);
+
+--todo en uno
+update programas p 
+set registros_i=(select count(medio)from registra r where medio='Internet' and r.codigo=p.codigo),
+    registros_t=(select count(medio)from registra r where medio='Teléfono' and r.codigo=p.codigo),
+    registros_p=(select count(medio)from registra r where medio='Tarjeta postal' and r.codigo=p.codigo);
 
 --6. Añade a la tabla "Clientes" una columna llamada "registrados" de tipo number que 
 --deberás actualizar con el número de registros que ha efectuado el cliente,
 --independientemente del medio por el que lo haya hecho.
+
+ALTER TABLE clientes ADD registrados number(10);
+update clientes c
+set registrados=(
+                select count (r.dni)
+                from registra r
+                where r.dni=c.dni);
